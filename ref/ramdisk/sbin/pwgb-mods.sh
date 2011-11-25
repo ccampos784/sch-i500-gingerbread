9b9ce9e7cd642fc9c21b671c83c0b2592799ac6e
Christopher Campos <ccampos784@gmail.com>
2011-11-25 14:06:40 -0500

    Remount system as RO after installing liblights.

--- ref/ramdisk/sbin/pwgb-mods.sh
+++ ref/ramdisk/sbin/pwgb-mods.sh
@@ -8,3 +8,7 @@ busybox mount -o remount,rw /system
 busybox cp -f /res/liblights/lights.s5pc110.so /system/lib/hw/lights.s5pc110.so
 busybox chown root.root /system/lib/hw/lights.s5pc110.so
 busybox chmod 0644 /system/lib/hw/lights.s5pc110.so
+
+# Remount /system ro
+busybox mount -o remount,ro /
+busybox mount -o remount,ro /system
