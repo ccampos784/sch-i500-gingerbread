0a228e16346a7731f4b72c285e29410d855c53d7
Christopher Campos <ccampos784@gmail.com>
2011-11-25 14:04:52 -0500

    Install liblights upon booting for BLN support.

---
+++ ref/ramdisk/sbin/pwgb-mods.sh
@@ -0,0 +1,10 @@
+#!/system/bin/sh
+
+# Remount /system rw
+busybox mount -o remount,rw /
+busybox mount -o remount,rw /system
+
+# Install liblights
+busybox cp -f /res/liblights/lights.s5pc110.so /system/lib/hw/lights.s5pc110.so
+busybox chown root.root /system/lib/hw/lights.s5pc110.so
+busybox chmod 0644 /system/lib/hw/lights.s5pc110.so
