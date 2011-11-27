2675d944d5f86ee9d3a60be0310d5a6fc6022ec7
Christopher Campos <ccampos784@gmail.com>
2011-11-26 21:57:35 -0500

    Updates to the cleaning script

--- clean.sh
+++ clean.sh
@@ -11,17 +11,21 @@
 #Set the working directory
 export CURRENTDIR=`pwd`
 
-#Remove BIN and OUT directories
+#Remove BIN and OUT directories, if they exist.
 cd $CURRENTDIR
-rm -r bin
-rm -r out
+if [ -d "bin" ]; then
+    rm -r bin
+fi
+if [ -d "out" ]; then
+    rm -r out
+fi
 
 #Switch to the kernel directory and clean up
 cd $CURRENTDIR/src/kernel
 make clean mrproper
 
 #Switch back to the working directory
-cd ../..
+cd $CURRENTDIR
 
 #EXIT
 exit
